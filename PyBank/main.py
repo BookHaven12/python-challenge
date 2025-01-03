593a7c0617f07d1493a98e80dd691f6a08c95411
BookHaven12 <iyermegan@gmail.com>
2025-01-02 17:20:41 -0700

    changed name of list from profit_losses to net_change_list,in Track the net change i replaced int(row[1]) with current_profit variable

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,12 +10,12 @@ records_path = os.path.join("Resources", "budget_data.csv")  # Input file path
 file_to_output = os.path.join("Analysis", "budget_analysis.txt")  # Output file path
 
 # Define variables to track the financial data
-total_months = 0
-total_net = 0
+total_months = 0 #total months in the data set
+total_net = 0 #net total amount of "Profit/Losses" over the entire period
 
 # Add more variables to track other necessary financial data
 date = []               #open list
-profit_losses = []     #open list to store changes
+net_change_list = []     #open list to store changes
 greatest_increase = ["", 0]     #list for date and value
 greatest_decrease = ["", 0]     #list for date and value
 greatest_increase = 0
@@ -42,13 +42,13 @@ with open(records_path) as financial_data:
     for row in reader:
         
         # Track the total
-        total_months +=1 #to increment the count for each row
+        total_months +=1 #to increment the count for each row by one
         current_profit = int(row[1])
         total_net += current_profit
 
         # Track the net change
-        net_change = int(row[1]) - previous_profit
-        profit_losses.append(net_change) #adds the calculated net change to the profit and loss list
+        net_change = current_profit - previous_profit
+        net_change_list.append(net_change) #adds the calculated net change to the profit and loss list
         previous_profit = int(row[1]) #assigns current profit/loss value to previous profit for the next calculation
     
         # Calculate the greatest increase in profits (month and amount)
@@ -59,7 +59,7 @@ with open(records_path) as financial_data:
 
 
 # Calculate the average net change across the months
-average_net_change = sum(profit_losses) / len(profit_losses)
+average_net_change = sum(net_change_list) / len(net_change_list)
 
 # Generate the output summary
 print(f"Financial Analysis")
